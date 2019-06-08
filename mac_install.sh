26e6c459eed8b2480ea3f60393d4c4d3e424ab8b
niya1123 <yoheikurioka@gmail.com>
2019-06-08 23:47:03 +0900

    修正

--- mac_install.sh
+++ mac_install.sh
@@ -5,7 +5,7 @@ brew install pyenv
 pyenv -v
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
 source ~/.bash_profile
 pyenv install --list
 sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
