0ebb649af30337ead8f2ef7cb82476799a785dc4
niya <yoheikurioka@gmail.com>
2019-06-28 13:38:54 +0900

    修正

--- ex1.py
+++ ex1.py
@@ -5,7 +5,5 @@ if(len(fruit_box) == value):
     print(f'箱の中には果物が{value}個あります')
 elif(len(fruit_box) < value):
     print(f'箱の中には果物が{value}個以下あります')    
-elif(len(fruit_box) > value):
-    print(f'箱の中には果物が{value}個以上あります')
 else:
-    print('変数:valueの値が不正です')
+    print(f'箱の中には果物が{value}個以上あります')
