1e862f658a316f225a81a241c0ef9bc726a07f60
niya1123 <yoheikurioka@gmail.com>
2019-06-29 17:51:31 +0900

    修正

--- for_ex.py
+++ for_ex.py
@@ -1,16 +1,13 @@
 url = 'http://kindai-csg.com/'
 
-# 普通のfor文
 for u in url:
     print(u)
 
-# for-else
 for u in url:
     print(u, end="")
 else:
     print()
     print('表示したやよ～')
 
-# 内包表記
 data = [x for x in range(0,10001) if x % 500 == 0]
 print(data)
